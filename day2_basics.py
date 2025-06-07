0f35328dad20e6b309e63deb0408398f00a62fcb
Pranav Tembare <pranav@Pranavs-MacBook-Air.local>
2025-06-07 16:10:45 +0530

    Day 2 - Python basics: variables, math, input

---
+++ day2_basics.py
@@ -0,0 +1,32 @@
+# Variables
+name = "Pranav"
+age = 21
+is_student = True
+print("Name:", name)
+print("Age:", age)
+print("Student:", is_student)
+
+# Math Operations
+a = 10
+b = 3
+print("Add:", a + b)
+print("Subtract:", a - b)
+print("Multiply:", a * b)
+print("Divide:", a / b)
+print("Remainder:", a % b)
+
+# Lists & Dictionaries
+tech_likes = ["AI", "Cybersecurity", "Cloud", "Python", "ML"]
+print("Tech Interests:", tech_likes)
+
+pranav_info = {
+    "name": "Pranav",
+    "city": "Nagpur",
+    "goal": "Become AI/ML Expert"
+}
+print(f"{pranav_info['name']} from {pranav_info['city']} wants to {pranav_info['goal']}.")
+
+# User Input
+your_name = input("Enter your name: ")
+your_age = input("Enter your age: ")
+print("Hello", your_name, "you are", your_age, "years old.")
